4f92e9bc75123fc4b87ecff0f977c939694b3c94
kulcsarrudolf <kulcsarrudolf@gmail.com>
2024-02-17 13:19:35 +0200

    Init .NET project

--- AddCustomPageSize.ps1
+++ AddCustomPageSize.ps1
@@ -1,31 +1,49 @@
-# Load the PDFCreator COM interface
-$pdfCreator = New-Object -ComObject PDFCreator.JobQueue
+Add-Type -TypeDefinition @"
+using System;
+using System.Runtime.InteropServices;
 
-try {
-    $pdfCreator.Initialize()
+public class PrinterConfig {
+    [DllImport("winspool.drv", CharSet = CharSet.Auto, SetLastError = true)]
+    public static extern bool AddForm(IntPtr hPrinter, uint Level, ref FORM_INFO_1 pForm);
 
-    $printJob = $pdfCreator.PrintJob
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+    public struct FORM_INFO_1 {
+        public uint Flags;
+        public string pName;
+        public SIZEL Size;
+        public RECTL ImageableArea;
+    }
 
-    $applicationSettings = $pdfCreator.GetApplicationSettings()
-    $profiles = $applicationSettings.GetProfiles()
+    [StructLayout(LayoutKind.Sequential)]
+    public struct SIZEL {
+        public int cx;
+        public int cy;
+    }
 
-    $customProfile = $profiles.CreateProfile("CustomProfile")
-    $customProfile.Name = "My Custom Profile"
-
-    $customProfile.PaperSize = New-Object -TypeName PDFCreator.ComImplementation.PaperSize
-    $customProfile.PaperSize.Width = 8
-    $customProfile.PaperSize.Height = 10
-    $customProfile.PaperSize.Unit = [PDFCreator.ComImplementation.PaperSizeUnits]::Inches
-
-    $profiles.SaveProfile($customProfile)
-
-    $applicationSettings.Save()
-
-    Write-Host "Custom profile created successfully."
+    [StructLayout(LayoutKind.Sequential)]
+    public struct RECTL {
+        public int left;
+        public int top;
+        public int right;
+        public int bottom;
+    }
 }
-catch {
-    Write-Error "An error occurred: $_"
-}
-finally {
-    $pdfCreator.ReleaseCom()
-}
+"@ -Namespace WinApi -UsingNamespace System.Runtime.InteropServices
+
+$printerName = "PDFCreator" # Specify the printer name
+$formName = "CustomSize" # Specify the custom form name
+
+$form = New-Object WinApi.PrinterConfig+FORM_INFO_1
+$form.Flags = 0
+$form.pName = $formName
+$form.Size = New-Object WinApi.PrinterConfig+SIZEL
+$form.Size.cx = 20000 # width in micrometers
+$form.Size.cy = 10000 # height in micrometers
+$form.ImageableArea = New-Object WinApi.PrinterConfig+RECTL
+$form.ImageableArea.left = 0
+$form.ImageableArea.top = 0
+$form.ImageableArea.right = 20000 # width in micrometers
+$form.ImageableArea.bottom = 10000 # height in micrometers
+
+# Add the custom paper size to the specified printer
+[WinApi.PrinterConfig]::AddForm($null, 1, [ref]$form)
